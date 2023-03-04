ba19cfc67d25b0b8c90f0569b003c65e50555ccf
Cecilie Seim <totilie8@gmail.com>
2023-03-04 15:52:16 +0100

    Basic sass structure connected to classNames

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -3,20 +3,36 @@ import './css/main.css';
 function App() {
   return (
     <div className="container">
-      <header className="pageHeader">
-        <nav className="navigation"></nav>
+      <header className="page-header">
+        <h1>Tekst</h1>
+       
       </header>
+      <nav className="navigation">
+          <a href="#noe" className='nav-links'>Noe</a>
+          <a href="#noe2" className='nav-links'>Noe2</a>
+        </nav>
       <aside className="search-bar">
-      <section className="search-section">
-          <form></form>
+        <h3>Søk</h3>
+        <section className="search-section">
+            <form></form>
         </section>
       </aside>
-      <main>
+      <main className='main-content'>
         <article className="page-content">
+          <h2>Innhold</h2>
+          <ul>
+            <li>Film 1</li>
+            <li>Film 2</li>
+            <li>Film 3</li>
+            <li>Film 4</li>
+          </ul>
 
         </article>
         
       </main>
+      <footer className='info-label'>
+        Laget av noen
+      </footer>
     
     </div>
   );
